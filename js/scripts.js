cac56fe8b8d0ff1bfe3cd0f6bec2abcd2f8723e9
Synth87 <141645597+Synth87@users.noreply.github.com>
2023-09-08 18:18:47 +0200

    Update scripts.js
    If applied, this commit will have updated the JavaScript-file.
    
    UPDATES
    - now the JS-file contains an array with 10 pokemon-objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,58 @@
-alert('Hello world');
+let pokemonList = [];
 
-let favoriteFood = 'pizza';
+/* Array with all pokemon-objects */
+pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Charmander',
+        height: 0.6,
+        types: ['fire']
+    },
+    {
+        name: 'Squirtle',
+        height: 0.5,
+        types: ['water']
+    },
+    {
+        name: 'Pidgey',
+        height: 0.3,
+        types: ['flying', 'normal']
+    },
+    {
+        name: 'Rattata',
+        height: 0.3,
+        types: ['normal']
+    },
+    {
+        name: 'Pikachu',
+        height: 0.4,
+        types: ['electric']
+    },
+    {
+        name: 'Nidoking',
+        height: 1.4,
+        types: ['ground', 'poison']
+    },
+    {
+        name: 'Zubat',
+        height: 0.8,
+        types: ['posion', 'flying']
+    },
+    {
+        name: 'Golduck',
+        height: 1.7,
+        types: ['water']
+    },
+    {
+        name: 'Poliwag',
+        height: 0.6,
+        types: ['water']
+    },
+];
 
-document.write(favoriteFood);
+console.log(pokemonList);
+console.log(`${pokemonList[0].name} ${pokemonList[0].height} ${pokemonList[0].types}`);
