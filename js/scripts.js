88c9ea365e1469b3ad7e05c2000fc44bc7d8d177
Synth87 <141645597+Synth87@users.noreply.github.com>
2023-09-16 07:00:56 +0200

    If applied, this commit will have updated the scripts.js
    - added an IIFE to wrap the whole pokemonList-array
    - added a return-statement with an object as a return value including 2 functions
    - changed the for-loop to iterate over the pokemonList-array to a forEach-loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,82 +1,104 @@
-// Initializing an empty array
-let pokemonList = [];
 
-/* Array with all pokemon objects */
-pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Charmander',
-        height: 0.6,
-        types: ['fire']
-    },
-    {
-        name: 'Squirtle',
-        height: 0.5,
-        types: ['water']
-    },
-    {
-        name: 'Pidgey',
-        height: 0.3,
-        types: ['flying', 'normal']
-    },
-    {
-        name: 'Rattata',
-        height: 0.3,
-        types: ['normal']
-    },
-    {
-        name: 'Pikachu',
-        height: 0.4,
-        types: ['electric']
-    },
-    {
-        name: 'Nidoking',
-        height: 1.4,
-        types: ['ground', 'poison']
-    },
-    {
-        name: 'Zubat',
-        height: 0.8,
-        types: ['posion', 'flying']
-    },
-    {
-        name: 'Golduck',
-        height: 1.7,
-        types: ['water']
-    },
-    {
-        name: 'Poliwag',
-        height: 0.6,
-        types: ['water']
-    },
-];
+// pokemon that gets added with the add-function
+let item = {
+    name: 'Snorlax',
+    height: 2.1,
+    types: ['normal']
+};
 
 
+// IIFE to limit the access to the pokemonList-array
+let pokemonRepository = (function () {
+
+    /* Array with all pokemon objects */
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Charmander',
+            height: 0.6,
+            types: ['fire']
+        },
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            types: ['water']
+        },
+        {
+            name: 'Pidgey',
+            height: 0.3,
+            types: ['flying', 'normal']
+        },
+        {
+            name: 'Rattata',
+            height: 0.3,
+            types: ['normal']
+        },
+        {
+            name: 'Pikachu',
+            height: 0.4,
+            types: ['electric']
+        },
+        {
+            name: 'Nidoking',
+            height: 1.4,
+            types: ['ground', 'poison']
+        },
+        {
+            name: 'Zubat',
+            height: 0.8,
+            types: ['posion', 'flying']
+        },
+        {
+            name: 'Golduck',
+            height: 1.7,
+            types: ['water']
+        },
+        {
+            name: 'Poliwag',
+            height: 0.6,
+            types: ['water']
+        },
+    ];
+
+    // function to return the pokemonList-array
+    function getAll() {
+        return pokemonList;
+    }
+
+    // function to add a pokemon to the pokemonList-array
+    function add(item) {
+        pokemonList.push(item);
+    }
+
+    // returns an object with 2 functions assigned as keys
+    return {
+        getAll: getAll,
+        add: add
+    };
+
+})();
 
 
-// declaring variables for the name, height and types of the pokemons
-let pokemonName = '';
-let pokemonHeight = null;
-let pokemonTypes = '';
+console.log(pokemonRepository.getAll());
 
-//for loop to iterate over every object in the pokemonList array starting at index 0
-for (i = 0; i < pokemonList.length; i++) {
+pokemonRepository.add(item);
 
-    // initializing the variables for the name, height and types of the pokemons to make the code more readable
-    pokemonName = pokemonList[i].name;
-    pokemonHeight = pokemonList[i].height;
-    pokemonTypes = pokemonList[i].types;
+console.log(pokemonRepository.getAll());
 
-    // condition to check if the height of the pokemon is greater than 1.4 m
-    if (pokemonList[i].height > 1.4) {
+
+
+//forEach-loop to iterate over every object in the pokemonList array starting at index 0.
+pokemonRepository.getAll().forEach(function (pokemon) {
+
+    if (pokemon.height > 1.7) {
         // output on the website's DOM if the pokemon is greater than 1.4 m
-        document.write(`<pre>Pokemon: ${pokemonName}    height: (${pokemonHeight})    types:(${pokemonTypes}) - Wow, that's big!</pre>`);
+        document.write(`<pre>Pokemon: ${pokemon.name}    height: (${pokemon.height})    types:(${pokemon.types}) - Wow, that's big!</pre>`);
     } else {
         // output on the website's DOM if the pokemon is smaller or equal to 1.4 m
-        document.write(`<pre>Pokemon: ${pokemonName}    height: (${pokemonHeight})    types:(${pokemonTypes})</pre>`);
+        document.write(`<pre>Pokemon: ${pokemon.name}    height: (${pokemon.height})    types:(${pokemon.types})</pre>`);
     }
-}
+});
