29a455a532c28208d19b91f7f89eff3b3264abc1
Synth87 <141645597+Synth87@users.noreply.github.com>
2023-09-20 08:41:18 +0200

    Update html, css, scripts.js
    If applied, this commit will have updated the html, css and scripts.js.
    
    Complete reworking of the design and functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -74,10 +74,50 @@ let pokemonRepository = (function () {
         pokemonList.push(item);
     }
 
+
+
+    function addListItem(pokemon) {
+
+        // selecting the pokemon-list
+        let pokemonList = document.querySelector('.pokemon-list');
+
+        // creating the variables for the list-item and the button. Together with the pokemonRepository-forEach-function every pokemon will get it's own button.
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+
+        // the text of the button gets the name of the pokemon
+        button.innerText = pokemon.name;
+        // the CSS class for the button is added
+        button.classList.add('button');
+
+        // the button is now appended to the listItem variable
+        listItem.appendChild(button);
+        // the listItem is appended to the pokemonList variable. This is the ul in the html document.
+        pokemonList.appendChild(listItem);
+
+        
+        // calls the showDetails function and passing the parameter 'pokemon' to it
+        // the abscence of the pokemon parameter is not an error. It is being accessed from the surrounding scope of the function (closure behaviour). The value of the pokemon variable is "captured" at the moment the event listener is set up (when calling it).
+        button.addEventListener('click', function(event){
+            showDetails(pokemon);
+        })
+
+    }
+
+    // show the name of the pokemon object in the console
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    }
+
+
+
     // returns an object with 2 functions assigned as keys
+    // optional: if the key and the value have the exact same name, then you could only write it once
+
     return {
         getAll: getAll,
-        add: add
+        add: add,
+        addListItem: addListItem
     };
 
 })();
@@ -85,6 +125,7 @@ let pokemonRepository = (function () {
 
 console.log(pokemonRepository.getAll());
 
+// Adding one pokemon to the pokemonList-array in the IIFE pokemonRepository
 pokemonRepository.add(item);
 
 console.log(pokemonRepository.getAll());
@@ -94,11 +135,7 @@ console.log(pokemonRepository.getAll());
 //forEach-loop to iterate over every object in the pokemonList array starting at index 0.
 pokemonRepository.getAll().forEach(function (pokemon) {
 
-    if (pokemon.height > 1.7) {
-        // output on the website's DOM if the pokemon is greater than 1.4 m
-        document.write(`<pre>Pokemon: ${pokemon.name}    height: (${pokemon.height})    types:(${pokemon.types}) - Wow, that's big!</pre>`);
-    } else {
-        // output on the website's DOM if the pokemon is smaller or equal to 1.4 m
-        document.write(`<pre>Pokemon: ${pokemon.name}    height: (${pokemon.height})    types:(${pokemon.types})</pre>`);
-    }
+    pokemonRepository.addListItem(pokemon);
+
 });
+
